ed6cbe9f508a2eb60d90305c9f1e6703cad1839c
Matt Todd <chiology@gmail.com>
2009-11-24 02:15:12 -0800

    Fixed variable mismatch.

--- queue_length.rb
+++ queue_length.rb
@@ -3,7 +3,7 @@ class QueueLength < Scout::Plugin
     length = %x{total=0; for i in `rabbitmqctl list_queues -p demandbase-api | grep tokens | grep usage | cut -f 2`; do total=$(($i+$total)); done; echo $total}.chomp.to_i
     report(:queued => length)
     if length > 0
-      alert("The queue currently has #{total} messages in it.")
+      alert("The queue currently has #{length} messages in it.")
     end
   end
 end
