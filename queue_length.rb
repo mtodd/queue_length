bb9b39258e40933b6b3fab95e2bcbdc5a230cc4b
Matt Todd <chiology@gmail.com>
2009-11-24 00:23:42 -0800

    Initial implementation of the queue length Scout monitoring plugin.

---
+++ queue_length.rb
@@ -0,0 +1,10 @@
+ass QueueLength < Scout::Plugin
+  def build_report
+    length = %x{total=0; for i in `rabbitmqctl list_queues -p demandbase-api | grep tokens | grep usage | cut -f 2`; do total=$(($i+$total)); done; echo $total}.chomp.to_i
+    report(:queued => length)
+    if length > 0
+      alert("The queue currently has #{total} messages in it.")
+    end
+  end
+end
+
